51b4bb6450d399e19665ec8d2f610b0dfd5645ee
hromaleo <hromadkoleos@gmail.com>
2019-09-20 10:15:34 +0200

    remaining TODOs done

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 ﻿# TODO
 
 1. ~Refaktorovat dle OOCSS (soubor leos.scss nesmí obsahovat .btn apod.)~
-2. Přidat do projektu Grid system, buď z internetu nebo z našeho devstacku
-3. Zbavit se javascriptu u kategorií odměn
+2. ~Přidat do projektu Grid system, buď z internetu nebo z našeho devstacku~
+3. ~Zbavit se javascriptu u kategorií odměn~
 4. ~Připravit responsivní breakpointy~
 5. ~Přepsat vše na mobile-first~
 6. ~Udělat menu v navigaci~
--- dist/js/secondaryNav.js
+++ dist/js/secondaryNav.js
@@ -10,6 +10,19 @@ function displaySecondaryNav() {
    const nav = document.querySelector(".secondary-navbar");
    console.log("To be displayed", nav);
    nav.style.display = "flex";
+   const navStyle = getComputedStyle(nav);
+   const navDelay = navStyle.transitionDuration;
+   const delayMs = cssTimeToMilliseconds(navDelay);
+   setTimeout(stretchIt, 0);
+   function stretchIt() { nav.style.height = "15rem" };
+   setTimeout(showIt, delayMs);
+   function showIt() { nav.style.opacity = "1"; }
+   setTimeout(changeTimeDown, delayMs * 2);
+   function changeTimeDown() {
+      const newNavDurationMs = delayMs / 2;
+      const newNavDuration = newNavDurationMs.toString() + "ms";
+      nav.style.transitionDuration = newNavDuration; //in order hiding to be faster
+   }
    const dropdowns = document.querySelectorAll(".main-navbar__link--dropdown, .secondary-navbar__item");
    console.log("Navigation menus and items to be close-able: ", dropdowns);
    for (let i = 0; i < dropdowns.length; i++) {
@@ -21,13 +34,50 @@ function displaySecondaryNav() {
 function hideSecondaryNav() {
    const nav = document.querySelector(".secondary-navbar");
    console.log("To be hidden", nav);
-   nav.style.display = "none";
+   nav.style.opacity = "0";
+   const navStyle = getComputedStyle(nav);
+   const navDelay = navStyle.transitionDuration;
+   const delayMs = cssTimeToMilliseconds(navDelay);
+   setTimeout(collapseIt, delayMs);
+   function collapseIt() { nav.style.height = "0"; }
+   setTimeout(changeTimeUp, delayMs * 2);
+   setTimeout(hideIt, delayMs * 2);
+   function changeTimeUp() {
+      const newNavDurationMs = delayMs * 2;
+      const newNavDuration = newNavDurationMs.toString() + "ms";
+      nav.style.transitionDuration = newNavDuration; //in order displaing to be slower
+   }
+   function hideIt() { nav.style.display = "none" }
    const dropdowns = document.querySelectorAll(".main-navbar__link--dropdown, .secondary-navbar__item");
    console.log("Navigation dropdown menus reset: ", dropdowns);
    for (let i = 0; i < dropdowns.length; i++) {
       dropdowns[i].removeEventListener("click", hideSecondaryNav)
       dropdowns[i].addEventListener("click", displaySecondaryNav)
    };
+}
+
+function cssTimeToMilliseconds(timeString) {
+   var num = parseFloat(timeString, 10),
+      unit = timeString.match(/m?s/),
+      milliseconds;
+
+   if (unit) {
+      unit = unit[0];
+   }
+
+   switch (unit) {
+      case "s": // seconds
+         milliseconds = num * 1000;
+         break;
+      case "ms": // milliseconds
+         milliseconds = num;
+         break;
+      default:
+         milliseconds = 0;
+         break;
+   }
+
+   return milliseconds;
 }
 
 
