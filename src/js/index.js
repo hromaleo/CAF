c6be261d8e866e41ef07ad928d2c8768145b10f0
hromaleo <hromadkoleos@gmail.com>
2019-10-09 22:10:38 +0200

    New index page with links to all built pages. Project now using devstack.

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,67 @@
-console.log("Is this thing on?");
+function createDropdownSecNav() {
+	const mainNavItems = document.querySelectorAll(".main-navbar__link--dropdown");
+	const subNavItems = document.querySelectorAll(".secondary-navbar__link");
+
+	for (let i = 0; i < mainNavItems.length; i++) {
+
+		mainNavItems[i].addEventListener("click", (e) => {
+			e.preventDefault();
+			const activeNavbar = document.querySelectorAll(".secondary-navbar--active");
+
+			const target = e.target;
+			const secondaryNav = target.nextElementSibling;
+
+			if (secondaryNav !== null && secondaryNav.classList.contains("secondary-navbar--active"))
+				secondaryNav.classList.remove("secondary-navbar--active");
+			else {
+				if (activeNavbar.length)
+					activeNavbar[0].classList.remove("secondary-navbar--active");
+				if (secondaryNav !== null)
+					secondaryNav.classList.add("secondary-navbar--active");
+			}
+		});
+	}
+
+	for (let i = 0; i < subNavItems.length; i++) {
+
+		subNavItems[i].addEventListener("click", (e) => {
+			// e.preventDefault();
+
+			const target = e.target;
+			const secondaryNav = target.closest(".secondary-navbar");
+
+			secondaryNav.classList.remove("secondary-navbar--active");
+		})
+	}
+}
+
+document.addEventListener("DOMContentLoaded", createDropdownSecNav);
+
+function createDropdownListItems() {
+	const dropdowns = document.querySelectorAll(".dropdown-list__link");
+
+	for (let i = 0; i < dropdowns.length; i++) {
+		dropdowns[i].addEventListener("click", (e) => {
+			e.preventDefault();
+			const activeListItems = document.querySelectorAll(".dropdown-list__content--active");
+
+			const target = e.target;
+			const listItem = target.nextElementSibling;
+
+			if (listItem !== null && listItem.classList.contains("dropdown-list__content--active"))
+				listItem.classList.remove("dropdown-list__content--active");
+			else {
+				if (activeListItems.length)
+					activeListItems[0].classList.remove("dropdown-list__content--active");
+				if (listItem !== null)
+					listItem.classList.add("dropdown-list__content--active");
+			}
+			return false;
+		})
+	}
+}
+
+
+
+document.addEventListener("DOMContentLoaded",
+	createDropdownListItems);
